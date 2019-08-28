144a6125e5a8f4043ca4df7291d6053f61b2a7a9
Christian Kittelmann <ckittelmann@samhammer.de>
2019-08-28 14:10:28 +0200

    fixed stylecop warnings

--- src/Samhammer.AspNetCore.HealthChecks.Prtg/Contact/PrtgResponseChannelValue.cs
+++ src/Samhammer.AspNetCore.HealthChecks.Prtg/Contact/PrtgResponseChannelValue.cs
@@ -1,7 +1,5 @@
 ﻿namespace Samhammer.AspNetCore.HealthChecks.Prtg.Contact
 {
-
-
     public abstract class PrtgResponseChannelValue<T> : PrtgResponseChannelValueBase where T : struct
     {
         public string Channel { get; set; }
